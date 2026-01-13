b1fbcc7aa71a964cd80abaa9857c8cd5ae268abf
Valeria Pavlova <valeriapavlova@iMac-de-Valeria.local>
2026-01-13 19:08:26 +0100

    Add parallax effect to slider card and update related styles

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+// js/index.js
+
 import { renderRelics } from './slider.js';
+import { setupRelicsParallax } from './parallax.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     renderRelics();
+    setupRelicsParallax();
 });
